da2afab637e8b41005c43e608e6e4ba5f78dbc80
tfwmoraa <jinnettemoraa@gmail.com>
2025-10-23 00:16:01 +0300

    submitted tdd events codealong

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+
 function App() {
   return (
-    <div></div>
-  )
+    <div>
+      <h1>Hi, I'm Jinnette</h1>
+    </div>
+  );
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,19 @@
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import App from "../App";
 
-import '@testing-library/jest-dom';
 
-// Code tests here
+test("displays a top-level heading with the text `Hi, I'm _______`", () => {
+  // Arrange
+  render(<App />);
+
+  // Act
+  const topLevelHeading = screen.getByRole("heading", {
+    name: /hi, i'm/i,
+    exact: false,
+    level: 1,
+  });
+
+  // Assert
+  expect(topLevelHeading).toBeInTheDocument();
+});
